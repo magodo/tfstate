b477c22fa8cb385dbebbdb8ff8922f52575e7adf
magodo <wztdyl@sina.com>
2024-08-23 15:51:10 +0800

    Update comment

--- terraform/jsonschema/doc.go
+++ terraform/jsonschema/doc.go
@@ -1,4 +1,4 @@
-// Package configschema is an adoption of a subset of the github.com/hashicorp/terraform/internal/configs/configschema@15ecdb66c84cd8202b0ae3d34c44cb4bbece5444.
+// Package configschema is an adoption of a subset of the github.com/hashicorp/terraform/internal/configs/configschema@92574a7811111f6afef4a16c9b72b8bd53e882e1.
 // It only focus on the implied type (and its dependencies) of the schema `Block` type. But instead of the `Block` defined internally by terraform core, it target
 // to the github.com/hashicorp/terraform-json.SchemaBlock.
 package jsonschema
